9606a37e5852a44110a887690a6a33432ec604bf
dancastrosouza <104075718+dancastrosouza@users.noreply.github.com>
2022-04-20 11:51:33 -0300

    Add files via upload

---
+++ aula 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+int main(){
+ //Variaveis
+ int x = 5;
+ int y = 10;
+ bool r;
+
+ x = x + 1; 
+ x += 1;
+
+  r = (x <= y);
+
+ cout << r;
+}
+
+
